8157a994b81250b07a610468432c47c9fb77e564
Martin George <marty@dingopaws.net>
2022-12-28 12:01:41 +0000

    Converted configuration into submodule

--- src/configuration/loggingConfiguration.ts
+++
@@ -1,3 +0,0 @@
-export const loggingConfiguration = {
-  logLevel: "debug"
-};
--- src/configuration/rooms/links/linkConfig.ts
+++
@@ -1,6 +0,0 @@
-export const linkConfig: { [roomName: string]: { [linkId: string]: string } } = {
-  W56N12: {
-    "6397f1bd30238608dae79135": "tx",
-    "639864121a5e460386cf8d54": "rx"
-  }
-};
